98785e5094d01b49f22b8fda9bdb7ee231152f89
Zoe Offermann <zoffermann15>
2020-03-13 23:13:02 -0700

    got flash cards to be added and viewable with prev and next button

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+struct Flashcard{
+    var question: String
+    var answer_rigth: String
+    var answer_wrong1: String
+    var answer_wrong2: String
+    
+}
+
 class ViewController: UIViewController {
             
     @IBOutlet weak var backLabel: UILabel!
@@ -17,6 +25,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     func buttonSetUp(button: UIButton){
         button.layer.borderWidth = 3.0
@@ -44,6 +55,8 @@ class ViewController: UIViewController {
         buttonSetUp(button: button1)
         buttonSetUp(button: button2)
         buttonSetUp(button: button3)
+        
+        updateFlashcard(question: "What is the capital of Argentina?", answer1: "Buenos Aires", answer2: "Córdoba", answer3: "Mar del Plata")
 
     }
 
@@ -65,14 +78,48 @@ class ViewController: UIViewController {
         frontLabel.isHidden = !frontLabel.isHidden
     }
     
-    func updateFlashcard(question:String,answer1:String, answer2: String, answer3: String){
-        frontLabel.text = question
-        backLabel.text = answer1
-        button1.setTitle(answer1, for: .normal)
-        button2.setTitle(answer2, for: .normal)
+    @IBAction func didTapPrev(_ sender: Any) {
+        if(currentIndex > 0){
+            currentIndex -= 1
+        }
+        updateLables()
+    }
+    
+    @IBAction func didTapNext(_ sender: Any) {
+        if(currentIndex < flashcards.count - 1){
+            currentIndex += 1
+        }
+        updateLables()
+    }
+    
+    func updateLables(){
+        let currentFlashcard = flashcards[currentIndex]
+        frontLabel.text = currentFlashcard.question
+        frontLabel.isHidden = false
+        backLabel.text = currentFlashcard.answer_rigth
+        button1.setTitle(currentFlashcard.answer_rigth, for: .normal)
+        button1.isHidden = false
+        button2.setTitle(currentFlashcard.answer_wrong1, for: .normal)
         button2.isHidden = false
-        button3.setTitle(answer3, for: .normal)
+
+        button3.setTitle(currentFlashcard.answer_wrong2, for: .normal)
         button3.isHidden = false
+
+    }
+    
+    func updateFlashcard(question:String,answer1:String, answer2: String, answer3: String){
+        let flashcard =  Flashcard(question: question, answer_rigth: answer1, answer_wrong1: answer2, answer_wrong2: answer3)
+       
+        frontLabel.text = flashcard.question
+        backLabel.text = flashcard.answer_rigth
+        button1.setTitle(flashcard.answer_rigth, for: .normal)
+        button2.setTitle(flashcard.answer_wrong1, for: .normal)
+        button2.isHidden = false
+        button3.setTitle(flashcard.answer_wrong2, for: .normal)
+        button3.isHidden = false
+        
+        flashcards.append(flashcard)
+        currentIndex = flashcards.count - 1
 
     }
     
